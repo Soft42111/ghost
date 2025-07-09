34d3a9cfcbbfec5f68172550cf34bd840b0fe873
soft42111 <soft4211@email.com>
2025-07-09 17:45:45 +0500

    Updated package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// Replace with your GitHub repo name
+const repoName = "YOUR_REPO_NAME";
+
 export default defineConfig(({ mode }) => ({
+  base: '/ghost/', // 👈 IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
